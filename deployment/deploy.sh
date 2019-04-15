c09e0eb112b6e9615122c71ac0a40dbd2fd3bc70
SantiagoRP7 <santiago.19988@gmail.com>
2019-04-15 14:49:17 -0500

    database position change and deploy.sh added

---
+++ deployment/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "starting the database"
+docker run --name=postgis -d -e POSTGRES_USER=juancamilo -e POSTGRES_PASS=24880312 -e POSTGRES_DBNAME=taxi -p 5433:5432 --restart=always kartoza/postgis
+echo "intantiating .sql"
+docker cp database.sql postgis:/database.sql
+docker exec postgis su - postgres -c "psql taxi -f ${SQLDIR}/database.sql" || true
+echo "iniciando el ApiRestFull api"
+docker run --rm -it --link=postgis:database --name api santiagorp7/apirest
+echo "iniciando el servidor frontend" 
+docker run --rm -it --link=api:apirest --name frontend santiagorp7/front
